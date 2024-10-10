ebe234909ed551b0e0539f560c7e4859d391ffd7
Mehmet Yağmur <mehmetygmrr02@gmail.com>
2024-10-11 00:10:25 +0300

    48

--- constructor-injection/src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
+++ constructor-injection/src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
@@ -1,7 +1,9 @@
 package com.luv2code.springcoredemo.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component
 public class TrackCoach implements Coach {
     @Override
