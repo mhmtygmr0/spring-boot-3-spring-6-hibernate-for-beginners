8b5f6f0bda3dc0ba4cbca4bc4ecc5fe12fac78d3
Mehmet <mehmetygmrr02@gmail.com>
2024-10-17 23:47:22 +0300

    80

--- 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/luv2code/cruddemo/dao/StudentDAO.java
+++ 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/luv2code/cruddemo/dao/StudentDAO.java
@@ -5,6 +5,7 @@ import com.luv2code.cruddemo.entity.Student;
 import java.util.List;
 
 public interface StudentDAO {
+
     void save(Student student);
 
     Student findById(Integer id);
@@ -12,4 +13,5 @@ public interface StudentDAO {
     List<Student> findAll();
 
     List<Student> findByLastName(String theLastName);
+
 }
