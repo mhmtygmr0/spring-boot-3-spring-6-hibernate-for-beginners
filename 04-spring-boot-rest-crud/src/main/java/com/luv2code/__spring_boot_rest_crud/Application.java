1e294b48950f85bd69299598b8b35695415820a0
Mehmet <mehmetygmrr02@gmail.com>
2024-10-21 23:15:36 +0300

    96

---
+++ 04-spring-boot-rest-crud/src/main/java/com/luv2code/__spring_boot_rest_crud/Application.java
@@ -0,0 +1,13 @@
+package com.luv2code.__spring_boot_rest_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ 04-spring-boot-rest-crud/src/test/java/com/luv2code/__spring_boot_rest_crud/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.__spring_boot_rest_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
