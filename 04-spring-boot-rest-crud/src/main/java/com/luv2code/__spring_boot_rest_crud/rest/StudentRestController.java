1286d8da7da3d60ebe68d43654afa61cc8f82a5c
Mehmet <mehmetygmrr02@gmail.com>
2024-10-25 22:49:25 +0300

    107

--- 04-spring-boot-rest-crud/src/main/java/com/luv2code/__spring_boot_rest_crud/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/src/main/java/com/luv2code/__spring_boot_rest_crud/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.luv2code.__spring_boot_rest_crud.rest;
 
 import com.luv2code.__spring_boot_rest_crud.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,6 +40,27 @@ public class StudentRestController {
 
         // just index into the list ... keep it simple for now
 
+        // check the studentId again list size
+
+        if (studentId >= theStudents.size() || (studentId < 0)) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
         return this.theStudents.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
