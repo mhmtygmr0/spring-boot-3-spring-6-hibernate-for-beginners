3280bd48b48b7970d73b4ad697f06c0eeef6010f
Mehmet <mehmetygmrr02@gmail.com>
2024-10-28 23:56:16 +0300

    109

--- 04-spring-boot-rest-crud/src/main/java/com/luv2code/__spring_boot_rest_crud/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/src/main/java/com/luv2code/__spring_boot_rest_crud/rest/StudentRestController.java
@@ -15,26 +15,29 @@ public class StudentRestController {
 
     private List<Student> theStudents;
 
-
     // define @PostConstruct to load the student data ... only once!
+
     @PostConstruct
     public void loadData() {
-        this.theStudents = new ArrayList<>();
 
-        this.theStudents.add(new Student("Poornima", "Patel"));
-        this.theStudents.add(new Student("Mario", "Rossi"));
-        this.theStudents.add(new Student("Mary", "Smith"));
+        theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Poornima", "Patel"));
+        theStudents.add(new Student("Mario", "Rossi"));
+        theStudents.add(new Student("Mary", "Smith"));
     }
 
 
     // define endpoint for "/students" - return a list of students
+
     @GetMapping("/students")
     public List<Student> getStudents() {
-        return this.theStudents;
+
+        return theStudents;
     }
 
-
     // define endpoint or "/students/{studentId}" - return student at index
+
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
 
@@ -42,17 +45,20 @@ public class StudentRestController {
 
         // check the studentId again list size
 
-        if (studentId >= theStudents.size() || (studentId < 0)) {
+        if ( (studentId >= theStudents.size()) || (studentId < 0)) {
             throw new StudentNotFoundException("Student id not found - " + studentId);
         }
 
-        return this.theStudents.get(studentId);
+        return theStudents.get(studentId);
     }
+
+    // Add an exception handler using @ExceptionHandler
 
     @ExceptionHandler
     public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
 
         // create a StudentErrorResponse
+
         StudentErrorResponse error = new StudentErrorResponse();
 
         error.setStatus(HttpStatus.NOT_FOUND.value());
@@ -62,5 +68,21 @@ public class StudentRestController {
         // return ResponseEntity
 
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    // add another exception handler ... to catch any exception (catch all)
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
