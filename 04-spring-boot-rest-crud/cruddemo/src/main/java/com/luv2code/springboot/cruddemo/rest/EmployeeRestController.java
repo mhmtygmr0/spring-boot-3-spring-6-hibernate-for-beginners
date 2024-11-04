da45c3232d5cde1cab72ff784cc796ab0047c924
Mehmet <mehmetygmrr02@gmail.com>
2024-11-04 23:53:40 +0300

    127

--- 04-spring-boot-rest-crud/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -2,10 +2,7 @@ package com.luv2code.springboot.cruddemo.rest;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,13 +23,26 @@ public class EmployeeRestController {
         return this.employeeService.findAll();
     }
 
-    @GetMapping("/employee/{employeeId}")
+    @GetMapping("/employees/{employeeId}")
     public Employee getEmployee(@PathVariable int employeeId) {
-        Employee theEmployee = employeeService.findById(employeeId);
+        Employee theEmployee = this.employeeService.findById(employeeId);
         if (theEmployee == null) {
             throw new RuntimeException("Employee id not found - " + employeeId);
         }
         return theEmployee;
     }
+
+    // add mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+
+        // also just in case they pass an id in JSON ... set id to 0
+        // this is to force a save of new item ... instead of update
+
+        employee.setId(0);
+
+        return this.employeeService.save(employee);
+    }
+
 
 }
