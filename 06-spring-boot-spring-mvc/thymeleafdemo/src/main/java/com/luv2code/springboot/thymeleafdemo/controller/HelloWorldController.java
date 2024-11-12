6702f3f48d8a1372629a88a97b60391c5d38da70
Mehmet <mehmetygmrr02@gmail.com>
2024-11-12 18:00:24 +0300

    173

--- 06-spring-boot-spring-mvc/thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -33,6 +34,21 @@ public class HelloWorldController {
 
         // create the message
         String result = "Yo! " + name;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String name, Model model) {
+
+        // convert the data to all caps
+        name = name.toUpperCase();
+
+        // create the message
+        String result = "Hey My Friend form v3! " + name;
 
         // add message to the model
         model.addAttribute("message", result);
