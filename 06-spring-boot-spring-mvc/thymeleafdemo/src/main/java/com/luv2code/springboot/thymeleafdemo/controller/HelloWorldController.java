b71c0dcea031c8324817d5d02415a761903b99ad
Mehmet <mehmetygmrr02@gmail.com>
2024-11-12 17:07:38 +0300

    170

--- 06-spring-boot-spring-mvc/thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +17,26 @@ public class HelloWorldController {
     // need a controller method to process the HTML form
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    // need a controller method to read  form data and
+    // add data to the model
+
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+        // read the request parameter from the HTML form
+        String name = request.getParameter("studentName");
+
+        // convert the data to all caps
+        name = name.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + name;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 
