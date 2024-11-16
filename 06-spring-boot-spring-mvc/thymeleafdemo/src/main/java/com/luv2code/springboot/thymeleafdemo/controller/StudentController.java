1658ce57cb157dc6260b7cd58209d39eedb17ed1
Mehmet <mehmetygmrr02@gmail.com>
2024-11-17 00:26:39 +0300

    183

--- 06-spring-boot-spring-mvc/thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,14 +1,20 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model model) {
@@ -18,6 +24,9 @@ public class StudentController {
         // add student object to the model
         model.addAttribute("student", student);
 
+        // add the list of countries to the model
+        model.addAttribute("countries", this.countries);
+
         return "student-form";
     }
 
@@ -25,7 +34,7 @@ public class StudentController {
     public String processForm(@ModelAttribute("student") Student student) {
         // log the input data
         System.out.println("Student: " + student.getFirstName() + " " + student.getLastName());
-        System.out.printf("Country: " + student.getCountry());
+        System.out.println("Country: " + student.getCountry());
 
         return "student-confirmation";
     }
