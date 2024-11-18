a5828c7a20090d24918fb1d6a3660c2e20c0c375
Mehmet <mehmetygmrr02@gmail.com>
2024-11-18 20:09:31 +0300

    185

--- 06-spring-boot-spring-mvc/thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
+++ 06-spring-boot-spring-mvc/thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
@@ -9,6 +9,7 @@ public class Student {
     private String firstName;
     private String lastName;
     private String country;
+    private String favoriteLanguage;
 
     public Student() {
     }
