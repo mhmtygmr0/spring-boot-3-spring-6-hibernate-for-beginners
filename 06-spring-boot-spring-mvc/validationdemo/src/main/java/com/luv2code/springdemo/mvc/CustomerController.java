88a409773c032be7fb8b0410288cec3a586012cd
Mehmet <mehmetygmrr02@gmail.com>
2024-11-18 21:56:48 +0300

    196

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
@@ -1,8 +1,14 @@
 package com.luv2code.springdemo.mvc;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.naming.Binding;
 
 @Controller
 public class CustomerController {
@@ -13,5 +19,15 @@ public class CustomerController {
         model.addAttribute("customer", new Customer());
 
         return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer customer,
+                              BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "customer-form";
+        } else {
+            return "customer-confirmation";
+        }
     }
 }
