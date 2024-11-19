bf5ac8ce3dc58ef12fc8bf56a3df47dd1e91b741
Mehmet <mehmetygmrr02@gmail.com>
2024-11-19 23:44:05 +0300

    201

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.luv2code.springdemo.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -14,6 +16,10 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName = "";
+
+    @Min(value = 0, message = "must be greater than or equal to Zero")
+    @Max(value = 10, message = "must be less than or equal to 10")
+    private int freePasses;
 
     public Customer() {
     }
