8415c73f92309357cffde1e0117febfcd513704a
Mehmet <mehmetygmrr02@gmail.com>
2024-12-12 23:21:46 +0300

    366

--- 10-spring-boot-aop/aopdemo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/aopdemo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,7 +1,10 @@
 package com.luv2code.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +14,14 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 
     @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint joinPoint) {
         System.out.println("\n====>>> Executing @Before advice on method");
+
+        // display the method signature
+        Signature methodSignature = (MethodSignature) joinPoint.getSignature();
+
+        System.out.println("Method:" + methodSignature);
+
+        // display method arguments
     }
 }
