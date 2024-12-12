033bda7cdbe7b475f4ea568b4156af7043b60bbd
Mehmet <mehmetygmrr02@gmail.com>
2024-12-12 23:35:17 +0300

    370

--- 10-spring-boot-aop/aopdemo/src/main/java/com/luv2code/aopdemo/dao/AccountDAOImpl.java
+++ 10-spring-boot-aop/aopdemo/src/main/java/com/luv2code/aopdemo/dao/AccountDAOImpl.java
@@ -3,11 +3,29 @@ package com.luv2code.aopdemo.dao;
 import com.luv2code.aopdemo.Account;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class AccountDAOImpl implements AccountDAO {
 
     private String name;
     private String serviceCode;
+
+    @Override
+    public List<Account> findAccounts() {
+        List<Account> accounts = new ArrayList<>();
+
+        Account account1 = new Account("John", "Silver");
+        Account account2 = new Account("Madhu", "Platinum");
+        Account account3 = new Account("Luca", "Gold");
+
+        accounts.add(account1);
+        accounts.add(account2);
+        accounts.add(account3);
+
+        return accounts;
+    }
 
     @Override
     public void addAccount(Account account, boolean cipFlag) {
