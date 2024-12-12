006bfa44e39db3c53a61b9312f22e5fe2db31a2d
Mehmet <mehmetygmrr02@gmail.com>
2024-12-12 16:01:00 +0300

    357

--- 10-spring-boot-aop/aopdemo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/aopdemo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -8,11 +8,6 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
-
-    // this is where we add all of our related advices for logging
-
-    // let's start with an @Before advice
-
     @Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
     private void forDaoPackage() {
 
@@ -21,5 +16,10 @@ public class MyDemoLoggingAspect {
     @Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
     public void beforeAddAccountAdvice() {
         System.out.println("\n====>>> Executing @Before advice on addAccount()");
+    }
+
+    @Before("forDaoPackage()")
+    public void performApiAnalytics() {
+        System.out.println("\n=====>>> Performing API analytics");
     }
 }
