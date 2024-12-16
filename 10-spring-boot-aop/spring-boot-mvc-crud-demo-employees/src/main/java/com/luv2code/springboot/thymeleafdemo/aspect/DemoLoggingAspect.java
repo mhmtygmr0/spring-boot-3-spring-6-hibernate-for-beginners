ca3e9727411f0f2b8ccc2f41601f57713f7d0667
Mehmet <mehmetygmrr02@gmail.com>
2024-12-16 20:28:02 +0300

    390

---
+++ 10-spring-boot-aop/spring-boot-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -0,0 +1,15 @@
+package com.luv2code.springboot.thymeleafdemo.aspect;
+
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class DemoLoggingAspect {
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+}
